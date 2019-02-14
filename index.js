595ddac87a3f864ee1590aebd83c2def55e47f08
Tommmy Collison <tommy@collison.ie>
2019-02-13 16:07:40 -0800

    updates

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 const knex = require('knex');
 const express = require('express');
 const server = express();
-const PORT = 7765;
+const PORT = process.env.PORT || 7766;
 
 server.use(express.json());
 
 
 server.get('/', (req, res) => {
+    
     res.send(`Welcome!`)
 })
 
-server.listen(PORT || 7766, () => {
-    console.log(`hi from port`)
+server.listen(process.env.PORT , () => {
+    console.log(`hi from port ${PORT}`)
+    console.log('env', process.env);
 })
+
+
+// server.listen(process.env.PORT || 3000)
+
+
